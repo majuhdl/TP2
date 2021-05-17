40b0bbdf2cf570fe1b8c1b5505faf756209087a4
majuhdl <34109238+majuhdl@users.noreply.github.com>
2021-05-17 15:07:24 -0300

    Update ReduceWords.scala

--- ReduceWords.scala
+++ ReduceWords.scala
@@ -2,6 +2,8 @@ package wc
 
 package object ReduceWords{
 	def reduce_words(list:List[(String, List[Int])]) : List[(String, Int)] = {
-		return list.map{case (s:String,l:List[Int]) =>(s, l.reduceLeft(_+_))}
+		return list.map{case (s:String,l:List[Int]) =>(s, l.reduceRight(_+_))}.groupBy(_._1).toList.map{case (s:String,l:List[(String,Int)]) =>(s, l.size)}.sortBy (-_._2)
+		
 	}
+
 }
